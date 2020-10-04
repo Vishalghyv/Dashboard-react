f220b24dc4bc53f5bf9f2622526502011063214d
Vishalghyv <vishal38785@gmail.com>
2020-10-04 16:04:50 +0530

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
 import {
+  Card,
   Badge,
   Button,
   Breadcrumb,
@@ -11,14 +12,35 @@ import {
   Modal
 } from "react-bootstrap";
 
+import logo from "./groffery.jpg";
+
 function SideBar() {
   return (
     <div className="sidebar">
-      <ListGroup variant="flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
+      <Card style={{ width: "100%", height: "100%" }}>
+        <Card.Img variant="top" src={logo} />
+        <Card.Header
+          style={{
+            padding: "25px 10px",
+            fontSize: "20px",
+            backgroundColor: "#d9d7d4"
+          }}
+        >
+          Dashboard
+        </Card.Header>
+        <ListGroup defaultActiveKey="#link1">
+          <ListGroup.Item action href="#link1">
+            Home
+          </ListGroup.Item>
+          <ListGroup.Item action>Upload New Flyer</ListGroup.Item>
+          <ListGroup.Item action>Analytics</ListGroup.Item>
+          <ListGroup.Item action>Another</ListGroup.Item>
+          <ListGroup.Item action>More setting</ListGroup.Item>
+          <ListGroup.Item action variant="warning">
+            LogOut
+          </ListGroup.Item>
+        </ListGroup>
+      </Card>
     </div>
   );
 }
