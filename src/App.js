686486541e06fdbae957d6b1a3454fc76479d230
Vishalghyv <vishal38785@gmail.com>
2020-10-04 14:53:30 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles.css";
+
+function SideBar() {
+  return <div> HEllo</div>;
+}
+
+function Home() {
+  return <div> HEllo</div>;
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <SideBar />
+      <Home />
+    </div>
+  );
+}
