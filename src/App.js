7c6182ea0a06387e2d7ca81d3afe2894185b9e44
Vishalghyv <vishal38785@gmail.com>
2020-10-04 15:02:49 +0530

    Init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./styles.css";
+import {
+  Badge,
+  Button,
+  Breadcrumb,
+  Form,
+  FormControl,
+  ListGroup,
+  ListGroupItem,
+  Modal
+} from "react-bootstrap";
 
 function SideBar() {
-  return <div> HEllo</div>;
+  return (
+    <div className="sidebar">
+      <ListGroup variant="flush">
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+    </div>
+  );
 }
 
 function Home() {
